7d03c2ad1ad030d5e2f16d68b36822c415cd3d87
plkujaw <kuba.jawor@gmail.com>
2020-09-02 20:38:10 +0100

    fixed typo in test description

--- leapYears.test.js
+++ leapYears.test.js
@@ -1,6 +1,6 @@
 const leapYear = require("./leapYears.js");
 
-test("should return true if a year is divisble by 400", () => {
+test("should return true if a year is divisible by 400", () => {
   expect(leapYear(2000)).toBe(true);
 });
 
