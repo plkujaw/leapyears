2e2fdbccd06779656fc5040bf0613be9aa904fc0
plkujaw <kuba.jawor@gmail.com>
2020-09-02 20:27:36 +0100

    All years divisible by 4 and not by 100 are leap years

--- leapYears.js
+++ leapYears.js
@@ -5,6 +5,9 @@ const leapYear = (year) => {
   if (year % 4 !== 0) {
     return false;
   }
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  }
 };
 
 module.exports = leapYear;
--- leapYears.test.js
+++ leapYears.test.js
@@ -7,3 +7,7 @@ test("should return true if a year is divisble by 400", () => {
 test("should return false if a year is not divisible by 4", () => {
   expect(leapYear(2009)).toBe(false);
 });
+
+test("should return true if a year is divisible by 4 but not divisible by 100", () => {
+  expect(leapYear(2004)).toBe(true);
+});
